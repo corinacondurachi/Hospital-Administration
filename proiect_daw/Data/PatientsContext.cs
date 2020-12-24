a96f8a4ed573a2155cf089eb21b2550de56940b7
corinacondurachi <45423375+corinacondurachi@users.noreply.github.com>
2020-12-24 18:01:36 +0200

    error unresolved

--- proiect_daw/Data/PatientsContext.cs
+++ proiect_daw/Data/PatientsContext.cs
@@ -1,9 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using DbContext = System.Data.Entity.DbContext;
 
 namespace proiect_daw.Data
 {
-    public class PatientsContext: DbContext
+    public class PatientsContext: IdentityDbContext
     {
         public PatientsContext(DbContextOptions <PatientsContext> options): base(options){}
         public System.Data.Entity.DbSet<Models.Patient> Patients { get; set; }
