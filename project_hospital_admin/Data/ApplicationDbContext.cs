b08c1fa6cc2485d6772d473d90621ee2f4da9323
corinacondurachi <45423375+corinacondurachi@users.noreply.github.com>
2020-12-27 18:57:10 +0200

    extended table

--- project_hospital_admin/Data/ApplicationDbContext.cs
+++ project_hospital_admin/Data/ApplicationDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using project_hospital_admin.Models;
 
 namespace project_hospital_admin.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        
+        //public DbSet <Patient> Patients { get; set; }
+        
     }
 }
