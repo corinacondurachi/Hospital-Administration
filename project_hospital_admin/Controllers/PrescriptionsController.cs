01e728e32381c9ac6ece8b812c5b264a733952b4
corinacondurachi <45423375+corinacondurachi@users.noreply.github.com>
2021-01-07 10:36:59 +0200

    edit + delete prescription

--- project_hospital_admin/Controllers/PrescriptionsController.cs
+++ project_hospital_admin/Controllers/PrescriptionsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
@@ -82,6 +83,82 @@ namespace project_hospital_admin.Controllers
             ViewData["prescriptions"] = _context.Prescriptions.ToList();
             return View();
         }
+        
+        // GET: /Prescriptions/EditPrescription/{id}
+        [HttpGet]
+        public ActionResult EditPrescription(int id)
+        {
+            var prescription = _context.Prescriptions.Find(id);
+
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+
+            return View(prescription);
+        }
+        
+        // POST: /Prescriptions/EditPrescriptions
+        [HttpPost]
+        public ActionResult EditPrescription(Prescription prescription)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var oldPrescription = _context.Prescriptions.Find(prescription.Id);
+
+                    if (oldPrescription == null)
+                    {
+                        return NotFound();
+                    }
+
+                    oldPrescription.CountryCode = prescription.CountryCode;
+                    oldPrescription.PhoneNumber = prescription.PhoneNumber;
+                    oldPrescription.EmailAddress = prescription.EmailAddress;
+                    oldPrescription.Doctor = prescription.Doctor;
+                    oldPrescription.Name = prescription.Name;
+                    oldPrescription.Sex = prescription.Sex;
+                    oldPrescription.Day = prescription.Day;
+                    oldPrescription.Month = prescription.Month;
+                    oldPrescription.Year = prescription.Year;
+                    oldPrescription.Diagnose = prescription.Diagnose;
+                    oldPrescription.Medication = prescription.Medication;
+                    
+                    
+                    TryUpdateModelAsync(oldPrescription);
+
+                    _context.SaveChanges();
+
+                    return RedirectToAction("ViewPrescriptions", "Prescriptions");
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new {error_message = e.Message});
+            }
+
+            return View(prescription);
+        }
+        
+        // GET: /Prescriptions/DeletePrescription/{id}
+        [HttpGet]
+        public ActionResult DeletePrescription(int id)
+        {
+            var prescription = _context.Prescriptions.Find(id);
+
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+
+            _context.Prescriptions.Remove(prescription);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("ViewPrescriptions", "Prescriptions");
+        }
+
 
 
 
